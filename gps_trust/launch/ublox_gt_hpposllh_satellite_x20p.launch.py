d51fdc2e4af8a914c05064fd7402c84615c05d57
Nick Hortovanyi <nick.hortovanyi@gmail.com>
2026-06-06 05:21:05 +1000

    Enable BDS geo PRN configuration

--- gps_trust/launch/ublox_gt_hpposllh_satellite_x20p.launch.py
+++ gps_trust/launch/ublox_gt_hpposllh_satellite_x20p.launch.py
@@ -36,6 +36,7 @@ def generate_launch_description():
         'CFG_MSGOUT_UBX_RXM_RAWX_USB': '1',
         'CFG_MSGOUT_UBX_RXM_SFRBX_USB': '1',
         'CFG_SIGNAL_BDS_ENA': 'true',
+        'CFG_BDS_USE_GEO_PRN': 'true',
         'CFG_SIGNAL_GLO_ENA': 'true',
         'CFG_SIGNAL_GAL_ENA': 'true',
         'CFG_SIGNAL_GPS_ENA': 'true',
